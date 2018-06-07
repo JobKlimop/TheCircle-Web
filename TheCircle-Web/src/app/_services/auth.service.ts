65c5b9eb4eb810892ce015e88a34f5d78702540a
Unknown <thomas_641991@hotmail.com>
2018-06-07 23:27:15 +0200

    Added AuthService

---
+++ TheCircle-Web/src/app/_services/auth.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+
+@Injectable()
+export class AuthService {
+
+
+  constructor(http: HttpClient) {
+
+  }
+
+  public login(username: string, password: string) {
+
+  }
+}
