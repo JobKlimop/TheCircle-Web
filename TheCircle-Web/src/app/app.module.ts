425419df098f2a137a6ad63ced4c78c7f17f1fd1
Bram <Walkerguy@users.noreply.github.com>
2018-06-08 10:56:22 +0200

    Revert "Merge pull request #1 from JobKlimop/feature/routing-and-menu"
    This reverts commit c4f5c95ddb984a698a6da744399ab6f1efa05a2a.

--- TheCircle-Web/src/app/app.module.ts
+++ TheCircle-Web/src/app/app.module.ts
@@ -1,26 +1,16 @@
-// Imports.
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { AppRoutingModule } from './app-routing-module';
 
-// Components.
 import { AppComponent } from './app.component';
 import { MainComponent } from './main/main.component';
-import { HeaderComponent } from './main/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MainComponent,
-    HeaderComponent
+    MainComponent
   ],
   imports: [
-    BrowserModule,
-    FormsModule,
-    HttpModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
   bootstrap: [AppComponent]
