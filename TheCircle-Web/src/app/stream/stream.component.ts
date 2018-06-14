7e19cce0c646ebfb11ff54a22fb8125bdf8ebf64
Bram <shep.ukn@gmail.com>
2018-06-14 23:57:30 +0200

    Stream page layout revamp

--- TheCircle-Web/src/app/stream/stream.component.ts
+++ TheCircle-Web/src/app/stream/stream.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-stream',
@@ -6,8 +7,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./stream.component.css']
 })
 export class StreamComponent implements OnInit {
+  public username: string;
+  public quote: string;
+  public viewers: string;
 
-  constructor() { }
+  constructor(private router: Router) {
+    // Set some default user information.
+    this.username = "Transparent person";
+    this.quote = "The best streamer on the circle.";
+    this.viewers = "21";
+   }
 
   ngOnInit() {
   }
